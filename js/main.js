405ac81cc9893fb0b86ed190c1748bb1e73ff3a2
mstrabach <mathieu.strabach@gmail.com>
2021-04-29 15:10:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,57 +13,76 @@ $(function() {
 
 
 $( "#openSeize" ).on( "click touchend", function() {
-	$(".project-details").attr('class','project-details');
-	$(".project-details").addClass("seize opened");
-	$(".project-details").html('').load("seize.html");
-	window.history.pushState("Seize", "Seize", "/website/seize");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").attr('class','project-details');
+			$(".project-details").addClass("seize opened");
+			$(".project-details").load("seize.html");
+			window.history.pushState("Seize", "Seize", "/website/seize");
+			$(".close").slideDown();
+		});
 });
+
 
 $( "#openRun" ).on( "click touchend", function() {
-	$(".project-details").attr('class','project-details');
-	$(".project-details").addClass("runmotion opened");
-	$(".project-details").html('').load("runmotion.html");
-	window.history.pushState("Run Motion", "Run Motion", "/website/runmotion");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").attr('class','project-details');
+			$(".project-details").addClass("runmotion opened");
+			$(".project-details").load("runmotion.html");
+			window.history.pushState("Run Motion", "Run Motion", "/website/runmotion");
+			$(".close").slideDown();
+		});
 });
 
+
 $( "#openRes" ).on( "click touchend", function() {
-	$(".project-details").attr('class','project-details');
-	$(".project-details").html('').load("responsage.html");
-	$(".project-details").addClass("responsage opened");
-	window.history.pushState("Responsage", "Responsage", "/website/responsage");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").attr('class','project-details');
+			$(".project-details").load("responsage.html");
+			$(".project-details").addClass("responsage opened");
+			window.history.pushState("Responsage", "Responsage", "/website/responsage");
+			$(".close").slideDown();
+		});
 });
 
 $( "#openCargo" ).on( "click touchend", function() {
-	$(".project-details").attr('class','project-details');
-	$(".project-details").addClass("cargo opened");
-	$(".project-details").load("cargo.html");
-	window.history.pushState("Cargo", "Cargo", "/website/cargo");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").attr('class','project-details');
+			$(".project-details").addClass("cargo opened");
+			$(".project-details").load("cargo.html");
+			window.history.pushState("Cargo", "Cargo", "/website/cargo");
+			$(".close").slideDown();
+		});
 });
+
 
 $( "#openMRCDS" ).on( "click touchend", function() {
-	$(".project-details").removeClass(" ");
-	$(".project-details").addClass("mrcds opened");
-	$(".project-details").html('').load("mrcds.html");
-	window.history.pushState("Mon Réseau Cancer du Sein", "Mon Réseau Cancer du Sein", "/website/mon-reseau-cancer-du-sein");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").removeClass(" ");
+			$(".project-details").addClass("mrcds opened");
+			$(".project-details").load("mrcds.html");
+			window.history.pushState("Mon Réseau Cancer du Sein", "Mon Réseau Cancer du Sein", "/website/mon-reseau-cancer-du-sein");
+			$(".close").slideDown();
+		});
 });
+
 
 $( "#openEwattch" ).on( "click touchend", function() {
-	$(".project-details").attr('class','project-details');
-	$(".project-details").addClass("ewattch opened");
-	$(".project-details").html('').load("ewattch.html");
-	window.history.pushState("Ewattch", "Ewattch", "/website/ewattch");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").attr('class','project-details');
+			$(".project-details").addClass("ewattch opened");
+			$(".project-details").load("ewattch.html");
+			window.history.pushState("Ewattch", "Ewattch", "/website/ewattch");
+			$(".close").slideDown();
+		});
 });
 
+
 $( "#openRestellini" ).on( "click touchend", function() {
-	$(".project-details").attr('class','project-details');
-	$(".project-details").addClass("restellini opened");
-	$(".project-details").html('').load("restellini.html");
-	window.history.pushState("Restellini", "Restellini", "/website/restellini");
-	$(".close").slideDown();
+	$(".project-details").empty().then(function() {
+			$(".project-details").attr('class','project-details');
+			$(".project-details").addClass("restellini opened");
+			$(".project-details").html('').load("restellini.html");
+			window.history.pushState("Restellini", "Restellini", "/website/restellini");
+			$(".close").slideDown();
+		});
 });
