d766c09770a1916c654df408f50ad83ba305d30b
mstrabach <mathieu.strabach@gmail.com>
2021-04-29 14:09:40 +0200

    gfgfgf

--- js/closing.js
+++ js/closing.js
@@ -1,10 +1,12 @@
 $( ".project-details .close" ).on( "click", function(event) {
 	history.pushState(null, null, "/website");
 	$(".project-details").removeClass("opened");
+	$(".close").hide();
 	event.stopPropagation();
 });
 
 window.addEventListener('popstate', function(event) {
     history.pushState(null, null, "/website");
 	$(".project-details").removeClass("opened");
+	$(".close").hide();
 }, false);
--- js/main.js
+++ js/main.js
@@ -16,40 +16,47 @@ $( "#openSeize" ).on( "click touchend", function() {
 	$(".project-details").addClass("seize opened");
 	$(".project-details").load("seize.html");
 	window.history.pushState("Seize", "Seize", "/website/seize");
+	$(".close").show();
 });
 
 $( "#openRun" ).on( "click touchend", function() {
 	$(".project-details").addClass("runmotion opened");
 	$(".project-details").load("runmotion.html");
 	window.history.pushState("Run Motion", "Run Motion", "/website/runmotion");
+	$(".close").show();
 });
 
 $( "#openRes" ).on( "click touchend", function() {
 	$(".project-details").addClass("responsage opened");
 	$(".project-details").load("responsage.html");
 	window.history.pushState("Responsage", "Responsage", "/website/responsage");
+	$(".close").show();
 });
 
 $( "#openCargo" ).on( "click touchend", function() {
 	$(".project-details").addClass("cargo opened");
 	$(".project-details").load("cargo.html");
 	window.history.pushState("Cargo", "Cargo", "/website/cargo");
+	$(".close").show();
 });
 
 $( "#openMRCDS" ).on( "click touchend", function() {
 	$(".project-details").addClass("mrcds opened");
 	$(".project-details").load("mrcds.html");
 	window.history.pushState("Mon Réseau Cancer du Sein", "Mon Réseau Cancer du Sein", "/website/mon-reseau-cancer-du-sein");
+	$(".close").show();
 });
 
 $( "#openEwattch" ).on( "click touchend", function() {
 	$(".project-details").addClass("ewattch opened");
 	$(".project-details").load("ewattch.html");
 	window.history.pushState("Ewattch", "Ewattch", "/website/ewattch");
+	$(".close").show();
 });
 
 $( "#openRestellini" ).on( "click touchend", function() {
 	$(".project-details").addClass("restellini opened");
 	$(".project-details").load("restellini.html");
 	window.history.pushState("Restellini", "Restellini", "/website/restellini");
+	$(".close").show();
 });
