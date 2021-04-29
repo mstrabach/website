64f68f256e8561a59cd26bd831bcb6c2ca58aa3a
mstrabach <mathieu.strabach@gmail.com>
2021-04-29 15:11:07 +0200

    Update closing.js

--- js/closing.js
+++ js/closing.js
@@ -1,7 +1,7 @@
 $( ".project-details .close" ).on( "click", function(event) {
 	history.pushState(null, null, "/website");
 	$(".project-details").attr('class','project-details closed');
-	$(".project-details").html('');
+	$(".project-details").empty();
 	$(".close").slideUp();
 	event.stopPropagation();
 });
@@ -9,6 +9,6 @@ $( ".project-details .close" ).on( "click", function(event) {
 window.addEventListener('popstate', function(event) {
     history.pushState(null, null, "/website");
 	$(".project-details").attr('class','project-details closed');
-	$(".project-details").html('');
+	$(".project-details").empty();
 	$(".close").slideUp();
 }, false);
